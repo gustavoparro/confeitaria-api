8a54b89bac404fa74902b63087bc2bcb488567bb
Gustavo Ferreira Parro <gustavo.ferreira.parro@gmail.com>
2021-10-24 13:06:12 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# confeitaria-api
+ 
---
+++ src/test/java/com/gustavoparro/confeitaria_api/ConfeitariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoparro.confeitaria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfeitariaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
